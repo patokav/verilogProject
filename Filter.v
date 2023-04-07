770a40cad7be4a5243845f9933569a01fb9a2431
Patokav <patokav007@gmail.com>
2023-04-07 12:00:16 +0100

    New v

--- Filter.v
+++ Filter.v
@@ -1,61 +1,91 @@
-module Filter ();
+module Filter(CLK,
+            RST,
+            IN,
+            OUT);
     
-input DATA;
 input CLK;
 input RST;
-input [127:0] DATAWORD;
+input IN;
 
-output reg [7:0]OUT;
+output reg [7:0] OUT;
 
+reg [7:0] MEM [0:63];
+reg [7:0] VAL;
 reg [7:0] COUNT;
+reg [11:0] AVG;
 
-reg [7:0] AVG [64];
+reg FLAG;
 
-reg [127:0] V1;
-reg [127:0] V2; //delayed by 1 clock
-reg [127:0] V3; //delayed by 2 clocks
+reg V1;
+reg V2; //delayed by 1 clock
+reg V3; //delayed by 2 clocks
+
+reg i;
 
 //holds for calcs
-reg [127:0] H2;
+reg H2;
 
 //filter = x1-2x2+x3
 
 always @(posedge CLK or negedge RST)
 begin
-    if(COUNT==63)
-    begin
-        V3<= V2; ///moving the values along the chain with 1 cycle delay
-        V2<= V1;
-        V1<= DATA; ///moving the values along the chain with 1 cycle delay
-    end
-    else
-    begin
-        COUNT++;
-    end
     if(RST==0)
     begin
-        OUT<=0;
+        FLAG <= 1'b0;
+        VAL<=0;
         V1<=0;
         V2<=0;
         V3<=0;
         COUNT<=0;
+        for (integer i = 0; i <=63; i++) 
+        begin
+            MEM[i] <= 0;
+        end
     end
     else
+    begin
+        if(COUNT==63)
+        begin
+            V3<= V2; ///moving the values along the chain with 1 cycle delay
+            V2<= V1;
+            V1<= IN; ///moving the values along the chain with 1 cycle delay
+            FLAG <= 1'b1;
+            COUNT<=0;
+        end
+        else
+        begin
+            COUNT++;
+        end
+    end 
 
 end
 
-always@(V1 or V2 or V3)
+always@(V2)
 begin
-    H2<= (V2<<1);
+    H2<= (V2<<1); //multiply by 2
 end
 
 always@(V1 or H2 or V3)
 begin
-    HOLD<=(V1-H2+V3);
+    VAL<=(V1-H2+V3);
 end
 
+always@ (COUNT)
+begin
+    i<=0;
+    MEM[COUNT]<=VAL; //using COUNT to iterate over array
 
 
+    if (FLAG) 
+    begin
+        AVG<=0;
+        for(i=0;i<=63;i++)
+        begin
+            AVG<=AVG+MEM[i];
+        end
 
+        OUT<=AVG>>6;
+    end
+end
 
 endmodule
--- Filter_TB.v
+++ Filter_TB.v
@@ -1,38 +1,40 @@
 // Testbench code for Filter & Decimator     64/44=16/11 //`time scale sets how long sim cycle lasts
 module Filter_TB();
  
-reg data;
 reg clk;
-reg reset_n;
+reg rst;
+reg in;
 reg [127:0] dataword;
 
 wire [7:0]out;
 
+Filter DUT(.CLK(clk),
+          .RST(rst),
+          .IN(in),
+          .OUT(out));
 
 initial
 begin
-  clk = 0;
-  data = 0;
-  reset_n = 1;
-  dataword = 0;
-
-// perform reset  
-  #100;
-  reset_n = 0;
-  #100;
-  reset_n = 1;
+   $dumpfile("dump.vcd");
+        $dumpvars(1);
+        clk<=0;
+  rst=1;
+  #10
+  rst=0;
+  #10
+  rst=1;
 
 // data upload 1    xxV0123456789ABC?
   dataword = 128'h02FF37abc326a7202381f0FF2a23f6be;
-  #6425
+  #10
   dataword = 128'h3864292ae9600269F0FF73abc3b7a721;
-  #6425
+  #10
   dataword = 128'h02395830aFF2a23FFbe3864792ae9603;
-  #6425
+  #10
   dataword = 128'h312899abc02138698329aef923073785;
-  #6425
+  #10
   dataword = 128'h0239830aFF2a23698329aef923073785;
-  #6425
+  #10
 
   $finish;
 end
@@ -40,7 +42,7 @@ end
 
 
 
-always #25
+always #5
 begin
   clk <= ~clk;
 end
@@ -48,9 +50,8 @@ end
 
 always @(posedge clk)
 begin
-  data <= dataword[127];
+  in <= dataword[127];
   dataword <= { dataword[126:0],1'b0}; //left wise bit shifts
-  //dataword <= (dataword << 1)  
 end
 
 
