d9aaeea48f13f3ac3fb90035a82f47c73651b051
PetriJF <jamespetri28@gmail.com>
2023-04-19 21:39:37 +0100

    Fixed all run errors and made sure the code runs properly. Bug fixing now

--- Filter.v
+++ Filter.v
@@ -1,91 +1,73 @@
-module Filter(CLK,
-            RST,
-            IN,
-            OUT);
+module Filter(input CLK,
+              input RST,
+              input IN,
+              output reg [7:0] OUT);
     
-input CLK;
-input RST;
-input IN;
+  reg [7:0] MEM [0:63];
+  reg [7:0] VAL;
+  reg [7:0] COUNT;
+  reg [11:0] AVG;
 
-output reg [7:0] OUT;
+  reg FLAG;
 
-reg [7:0] MEM [0:63];
-reg [7:0] VAL;
-reg [7:0] COUNT;
-reg [11:0] AVG;
+  reg V1;
+  reg V2; //delayed by 1 clock
+  reg V3; //delayed by 2 clocks
 
-reg FLAG;
+  integer i;
 
-reg V1;
-reg V2; //delayed by 1 clock
-reg V3; //delayed by 2 clocks
+  //holds for calcs
+  reg H2;
 
-reg i;
+  //filter = x1-2x2+x3
 
-//holds for calcs
-reg H2;
-
-//filter = x1-2x2+x3
-
-always @(posedge CLK or negedge RST)
-begin
-    if(RST==0)
-    begin
-        FLAG <= 1'b0;
-        VAL<=0;
-        V1<=0;
-        V2<=0;
-        V3<=0;
-        COUNT<=0;
-        for (integer i = 0; i <=63; i++) 
-        begin
-            MEM[i] <= 0;
-        end
+  always @ (posedge CLK or negedge RST) begin
+    if(RST == 0) begin
+      FLAG <= 1'b0;
+      VAL <= 0;
+      V1 <= 0;
+      V2 <= 0;
+      V3 <= 0;
+      H2 <= 0;
+      COUNT <= 0;
+      
+      for (i = 0; i < 64; i=i+1) begin
+        MEM[i] <= 0;
+      end
     end
-    else
-    begin
-        if(COUNT==63)
-        begin
-            V3<= V2; ///moving the values along the chain with 1 cycle delay
-            V2<= V1;
-            V1<= IN; ///moving the values along the chain with 1 cycle delay
-            FLAG <= 1'b1;
-            COUNT<=0;
-        end
-        else
-        begin
-            COUNT++;
-        end
-    end 
-
-end
-
-always@(V2)
-begin
-    H2<= (V2<<1); //multiply by 2
-end
-
-always@(V1 or H2 or V3)
-begin
-    VAL<=(V1-H2+V3);
-end
-
-always@ (COUNT)
-begin
-    i<=0;
-    MEM[COUNT]<=VAL; //using COUNT to iterate over array
-
-
-    if (FLAG) 
-    begin
-        AVG<=0;
-        for(i=0;i<=63;i++)
-        begin
-            AVG<=AVG+MEM[i];
-        end
-
-        OUT<=AVG>>6;
+    else begin
+      if(COUNT==63) begin
+        V3 <= V2; // moving the values along the chain with 1 cycle delay
+        V2 <= V1;
+        V1 <= IN; // moving the values along the chain with 1 cycle delay
+        FLAG <= 1'b1;
+        COUNT <= 0;
+      end
+      else begin
+        COUNT++;
+      end
     end
-end
+  end
 
+  always @ (V2) begin
+    H2 <= (V2 << 1); //multiply by 2
+  end
+
+  always @ (V1 or H2 or V3) begin
+    VAL <= (V1 - H2 + V3);
+  end
+
+  always @ (COUNT) begin
+    i <= 0;
+    MEM[COUNT] <= VAL; //using COUNT to iterate over array
+
+    if (FLAG) begin
+      AVG <= 0;
+      for(i=0; i < 64; i = i + 1) begin
+        AVG <= AVG + MEM[i];
+      end
+
+      OUT <= AVG >> 6;
+    end
+  end
 endmodule
--- Filter_TB.v
+++ Filter_TB.v
@@ -1,67 +1,57 @@
 // Testbench code for Filter & Decimator     64/44=16/11 //`time scale sets how long sim cycle lasts
 module Filter_TB();
  
-reg clk;
-reg rst;
-reg in;
-reg [127:0] dataword;
+  reg clk;
+  reg rst;
+  reg in;
+  reg [127:0] dataword;
 
-wire [7:0]out;
+  wire [7:0]out;
 
-Filter DUT(.CLK(clk),
-          .RST(rst),
-          .IN(in),
-          .OUT(out));
+  Filter DUT(
+    .CLK(clk),
+    .RST(rst),
+    .IN(in),
+    .OUT(out)
+  );
 
-initial
-begin
-   $dumpfile("dump.vcd");
-        $dumpvars(1);
-        clk<=0;
-  rst=1;
-  #10
-  rst=0;
-  #10
-  rst=1;
+  initial begin
+    $monitor ("reset = %d in = %b out = %b", rst, in, out);
+    $dumpfile("dump.vcd");
+    $dumpvars(2);
+    
+  	clk = 1'b0;
+    in = 1'b0;
+    dataword = 0;
+    rst = 1'b1;
+    #100
+    rst <= 1'b0;
+    #100
+    rst <= 1'b1;
 
-// data upload 1    xxV0123456789ABC?
-  dataword = 128'h02FF37abc326a7202381f0FF2a23f6be;
-  #300
-  dataword = 128'h3864292ae9600269F0FF73abc3b7a721;
-  #300
-  dataword = 128'h02395830aFF2a23FFbe3864792ae9603;
-  #300
-  dataword = 128'h312899abc02138698329aef923073785;
-  #300
-  dataword = 128'h0239830aFF2a23698329aef923073785;
-  #10
+    // data upload 1    xxV0123456789ABC?
+    dataword <= 128'h02FF37abc326a7202381f0FF2a23f6be;
+    #6425
+    //dataword = 128'h3864292ae9600269F0FF73abc3b7a721;
+    //#200
+    //dataword = 128'h02395830aFF2a23FFbe3864792ae9603;
+    //#200
+    //dataword = 128'h312899abc02138698329aef923073785;
+    //#200
+    //dataword = 128'h0239830aFF2a23698329aef923073785;
+    //#10
 
-  $finish;
-end
+    #10 $finish;
+  end
 
+  always #5
+  begin
+    clk <= ~clk;
+  end
 
-
-
-always #5
-begin
-  clk <= ~clk;
-end
-
-
-always @(posedge clk)
-begin
-  in <= dataword[127];
-  dataword <= { dataword[126:0],1'b0}; //left wise bit shifts
-end
-
-
-
-// module group2 group2_code(.clk (clk),
-//                           .data (bitdata),
-//                           .reset_n (reset_n),
-//                           etc etc etc
-//                                              )
-
-
+  always @(posedge clk) begin
+    in <= dataword[127];
+    dataword <= { dataword[126:0], 1'b0 }; //left wise bit shifts
+    //$display ("in = %b", in);
+  end
 endmodule
-
